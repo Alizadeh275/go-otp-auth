0c1d0192a6687c3816389a2f1db2f2a6527a3899
Alizadeh275 <sajjadalizadeh275800@gmail.com>
2025-09-16 06:47:01 -0400

    added tokenExpiry var & SetTokenExpiry function

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,42 +1,48 @@
 package auth
 
 import (
-    "errors"
-    "time"
+	"errors"
+	"time"
 
-    "github.com/golang-jwt/jwt/v5"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 var jwtSecret []byte
+var tokenExpiry = time.Hour // default 1 hour
 
 func InitJWT(secret string) {
-    jwtSecret = []byte(secret)
+	jwtSecret = []byte(secret)
+}
+
+func SetTokenExpiry(d time.Duration) {
+	tokenExpiry = d
 }
 
 func CreateToken(userID int64) (string, error) {
-    claims := jwt.MapClaims{
-        "sub": userID,
-        "exp": time.Now().Add(24 * time.Hour).Unix(),
-    }
-    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-    return token.SignedString(jwtSecret)
+	claims := jwt.MapClaims{
+		"sub": userID,
+		"exp": time.Now().Add(tokenExpiry).Unix(),
+		"iat": time.Now().Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
 }
 
 func ParseToken(tokenStr string) (int64, error) {
-    token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
-        if t.Method.Alg() != jwt.SigningMethodHS256.Alg() {
-            return nil, errors.New("unexpected signing method")
-        }
-        return jwtSecret, nil
-    })
-    if err != nil {
-        return 0, err
-    }
-    if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-        switch v := claims["sub"].(type) {
-        case float64:
-            return int64(v), nil
-        }
-    }
-    return 0, errors.New("invalid token")
+	token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+		if t.Method.Alg() != jwt.SigningMethodHS256.Alg() {
+			return nil, errors.New("unexpected signing method")
+		}
+		return jwtSecret, nil
+	})
+	if err != nil {
+		return 0, err
+	}
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		switch v := claims["sub"].(type) {
+		case float64:
+			return int64(v), nil
+		}
+	}
+	return 0, errors.New("invalid token")
 }
